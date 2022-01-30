a26d67ad4f20f0ab77d668a28a1aafb0b226bc15
Andriy Kryvopysha <logan1kry@gmail.com>
2022-01-30 19:50:54 +0200

    Done Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
 console.log(galleryItems);
+import minCss from "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(picture => {
+    
+    const linkEL = document.createElement('a');
+    linkEL.classList.add('gallery__item');
+  linkEL.href = picture.original;
+ 
+
+    const imageEL = document.createElement('img');
+    imageEL.classList.add('gallery__image');
+    imageEL.src = picture.preview;
+    imageEL.alt = picture.description;
+
+    linkEL.append(imageEL);
+
+    galleryEl.append(linkEL)
+
+})
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
